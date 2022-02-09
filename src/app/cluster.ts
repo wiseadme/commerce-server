34dcd257c2d3d446fcc22d3988cec5f2528f6bae
Anar <wiseadme@gmail.com>
2022-02-09 15:37:02 +0300

    ref: add Run class

--- src/app/cluster.ts
+++ src/app/cluster.ts
@@ -1,19 +1,40 @@
-import cluster from 'cluster'
-import { server } from './server'
-import consola from 'consola'
+import cluster from 'cluster';
+import { server } from './server';
+import consola from 'consola';
+import cCPUs from 'os';
+import { Application } from 'express';
 
-const cCPUs = require('os').cpus().length
+class Cluster {
+  cCPUs: number;
+  server: Application;
 
-if (cluster.isMaster) {
-  for (let i = 0; i < cCPUs; i++) {
-    cluster.fork()
+  constructor(server){
+    this.cCPUs = cCPUs.cpus().length;
+    this.server = server;
+
+    this.initApp();
   }
-  cluster.on('online', (worker) => {
-    consola.success('Worker ' + worker.process.pid + ' is online.')
-  })
-  cluster.on('exit', (worker, code, signal) => {
-    consola.info('worker ' + worker.process.pid + ' died.')
-  })
-} else {
-  server.listen()
+
+  initApp(){
+    if (cluster.isMaster) this.initClusters();
+    else this.initServer();
+  }
+
+  initClusters(){
+    for (let i = 0; i < this.cCPUs; i += 1) {
+      cluster.fork();
+    }
+    cluster.on('online', (worker) => {
+      consola.success('Worker ' + worker.process.pid + ' is online.');
+    });
+    cluster.on('exit', (worker, code, signal) => {
+      consola.info('worker ' + worker.process.pid + ' died.');
+    });
+  }
+
+  initServer(){
+    this.server.listen();
+  }
 }
+
+const app = new Cluster(server);
